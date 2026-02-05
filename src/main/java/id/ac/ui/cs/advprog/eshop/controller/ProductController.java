4bb6d329a90aaf84f1db460ced81d9ebe4337edf
nathanaeru <nathanael.leander@ui.ac.id>
2026-02-06 06:53:38 +0700

    Add edit product functionality

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/product")
@@ -22,6 +24,8 @@ public class ProductController {
     @GetMapping("/create")
     public String createProductPage(Model model) {
         Product product = new Product();
+        String uniqueId = UUID.randomUUID().toString();
+        product.setProductId(uniqueId);
         model.addAttribute("product", product);
         return "create_product";
     }
@@ -37,5 +41,21 @@ public class ProductController {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
         return "list_product";
+    }
+
+    @GetMapping("/edit/{productId}")
+    public String editProductPage(@PathVariable("productId") String productId, Model model) {
+        Product product = service.findAll().stream()
+                .filter(p -> p.getProductId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + productId));
+        model.addAttribute("product", product);
+        return "edit_product";
+    }
+
+    @PostMapping("/edit")
+    public String editProductPost(@ModelAttribute Product product, Model model) {
+        service.edit(product);
+        return "redirect:list";
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Repository
 public class ProductRepository {
@@ -18,5 +19,22 @@ public class ProductRepository {
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(id)) {
+                return product;
+            }
+        }
+        throw new NoSuchElementException("Product with ID " + id + " not found.");
+    }
+
+    public Product edit(Product updatedProduct) {
+        String id = updatedProduct.getProductId();
+        Product oldProduct = findById(id);
+        oldProduct.setProductName(updatedProduct.getProductName());
+        oldProduct.setProductQuantity(updatedProduct.getProductQuantity());
+        return oldProduct;
     }
 }
